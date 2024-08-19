733f199014e060d36f635143b309b3c14112c591
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-08-19 11:36:58 +0800

    Update views.py

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,3 +1,41 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Category, Tag, Post
+
+
+def index_view(request):
+    pass
+
+
+def _common_get_list(request):
+    pass
+
+
+def category_list_view(request, id_var):
+    pass
+
+
+def tag_list_view(request, id_var):
+    pass
+
+
+def post_list_view(request: WSGIRequest):
+    print(f"post_list_view:")
+    if request.method == "GET":
+        res = Post.get_queryset_mixin()
+        print(f"res: {res}")
+        return HttpResponse(content=b"get_post_list_view")
+    else:
+        raise Exception()
+
+
+def post_detail_view(request: WSGIRequest, id_var):
+    print(f"post_detail_view:")
+    if request.method == "GET":
+        res = Post.get_queryset_mixin(dict(id=id_var))
+        print(f"res: {res}")
+        return HttpResponse(content=b"get_post_list_view")
+    else:
+        raise Exception()
