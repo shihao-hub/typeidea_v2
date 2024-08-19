529739f91f1066f4aa661060f1c1622c24b461bd
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-08-19 11:37:55 +0800

    Update admin.py

--- typeidea/comment/admin.py
+++ typeidea/comment/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
 from comment.models import Comment
+from shared.modeladmin_mixin import SetOwnerToCurrentUserMixin
 
 
 @admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(SetOwnerToCurrentUserMixin, admin.ModelAdmin):
     list_display = ("target", "content", "nickname", "website", "email", "status", "created_time")
+
+    def save_model(self, *args, **kwargs):
+        return self.save_model_mixin(*args, **kwargs)
